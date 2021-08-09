b658429700a0aaa40c9b286a194d5cac59d7d05a
Jhon-bit-a <joderconella@gmail.com>
2021-08-09 15:21:56 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', 'ProductController@index');
+Route::get('/proveedor', 'ProveedorController@index');
